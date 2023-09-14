b4604ca360af47a591a8fb6e01c47195755c0c2f
unknown <you@example.com>
2023-09-14 22:45:44 +0600

    create fake data for my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Course from './Component/Course/Course'
 
 function App() {
 
   return (
     <>
 
-      <h1>Vite + React</h1>
+      <h1 className='text-4xl text-center font-semibold'>Course Registration</h1>
+
+      <Course></Course>
 
     </>
   )
---
+++ src/Component/Courses/Courses.jsx
@@ -0,0 +1,19 @@
+
+const Courses = () => {
+    return (
+        <div>
+            <div className='border-2 w-96 border-blue-100 p-3'>
+                <img src="" alt="this is img" />
+                <h2 className='text-2xl font-medium'>this is a taitel</h2>
+                <p className=''>this is a pragraph</p>
+                <div className="flex justify-around">
+                    <p>Price:</p>
+                    <p>Credit:</p>
+                </div>
+                <button className='bg-green-950 w-5/6 border rounded'>Select</button>
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
